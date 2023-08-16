016ca089c564c20a42bddff4a1ef319838ec6cb9
Putinka1993 <lipkin1993vladislav@gmail.com>
2023-08-16 15:25:47 +0300

    readYY

--- work_search_ip/serach_ip_v1_1.py
+++ work_search_ip/serach_ip_v1_1.py
@@ -1,4 +1,7 @@
 
+
+
+
 from tkinter import *
 from tkinter import ttk
 import os
@@ -12,35 +15,24 @@ class app(tk.Frame):
         self.root.geometry("250x200") 
         
         self.entry = ttk.Entry()
-        self.entry.pack(anchor=NW, padx=6, pady=6)      
+        self.entry.pack(anchor=NW, padx=6, pady=6)
+        
         self.btn = ttk.Button(text="search ip", command=self.click)
         self.btn.pack(anchor=NW, padx=6, pady=6)
         self.btn.bind('<Return>',lambda e, f = "Verdana": self.click(e, f))
         self.label = ttk.Label(text="write ip")
         self.label.pack(anchor=NW, padx=6, pady=6)
-
-        
-        self.root.mainloop()
+    self.root.mainloop()
 
     def click (self, e, f):      
+        count = 0
         hostname = self.entry.get()
-        
-        for i in range(10):
-            time.sleep(5)
-            self.response = os.system("ping -c 1 " + hostname)
-            if self.response == 0:
+        response = os.system("ping -c 1 " + hostname)
+        for i in range(5):
+            time.sleep(1)
+
+            if response == 0:
                 self.label.config(text = str("online"))  
             else:
                 self.label.config(text = str("offline"))
             self.root.update()
-
-    
-    
-
-    #window.destroy()
-    
-
-
-#search_ip = "test"
-
-app()
